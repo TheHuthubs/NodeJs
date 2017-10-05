f7296ada2321f7b67cb6a14a6fccdd34f00551ee
eldadyaakobi <eldad.yaakobi@gmail.com>
2017-10-06 00:26:33 +0300

    adding html http://localhost:8888/

--- LightLedServer.js
+++ LightLedServer.js
@@ -1,21 +1,28 @@
 var net = require('net');
 var http = require('http');
 var five = require("johnny-five");
+var fs = require('fs');
+var path = require('path')
+
 
 var board = new five.Board();
 
 var HOST = '127.0.0.1';
 var PORT = 6969;  // TCP listen PORT
 var LedData = 0;  // how many LEDs should be turned on
-var message; // To http response
+var message = 1; // To http response
 
+fs.readFile('LedStatus.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
 // HTTP server
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(message);
-    res.end();
-
-}).listen(8888);
+    http.createServer(function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(message + html);
+            res.end();
+    }).listen(8888);
+});
 
 // TCP Server to
 net.createServer(function (socket) {
@@ -25,7 +32,7 @@ net.createServer(function (socket) {
 
     // handle incoming data - data event handler
     socket.on('data', function (data) {
-        message = "[Client -> Server] A new msg was received:" +  data + "LEDs will be light out";
+        message = '[Client -> Server] A new msg was received: ' +  data + ' LEDs will be light out';
         console.log(message);
         LedData = data;
         socket.write(data)
@@ -44,7 +51,7 @@ board.on("ready", function() {
     console.log("Ready ya hothubbbb!");
     var led = new five.Led(13);
     console.log("light:" + LedData);
-    led.blink(5000);
+    led.blink(message*1000);
 
     this.on("exit", function() {
         led.off();
